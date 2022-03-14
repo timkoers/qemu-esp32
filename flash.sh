af186b2efb8212d1cee2bb3c9f9d12afe99e8bf4
Tim Koers <tim.koers@live.nl>
2022-03-14 11:31:13 +0100

    Fixed flash script

--- flash.sh
+++ flash.sh
@@ -3,7 +3,12 @@ if [ -z "$1" ]
     echo "eg: flash.sh application.bin"
     exit
 fi
+echo "Creating file"
 dd if=/dev/zero bs=1M count=4 of=./flash.bin
-dd if=./program/build/bootloader/bootloader.bin bs=1 count=$(stat -c%s ./program/build/bootloader/bootloader.bin) seek=$((16#1000)) conv=notrunc of=./flash.bin
-dd if=./program/build/partitions_singleapp.bin bs=1 count=$(stat -c%s ./program/build/partitions_singleapp.bin) seek=$((16#8000)) conv=notrunc of=./flash.bin
+echo "Writing bootloader"
+dd if=./bootloader.bin bs=1 count=$(stat -c%s ./bootloader.bin) seek=$((16#1000)) conv=notrunc of=./flash.bin
+echo "Writing partitions"
+dd if=./partitions.bin bs=1 count=$(stat -c%s ./partitions.bin) seek=$((16#8000)) conv=notrunc of=./flash.bin
+echo "Finishing file"
 dd if=$1 bs=1 count=$(stat -c%s $1) seek=$((16#10000)) conv=notrunc of=./flash.bin
+echo "Finished file"
